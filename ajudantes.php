0c42dff27333802265cecb8b2a07b811f0ac4a5a
Kauan Holtz <129107521+KauanHoltz@users.noreply.github.com>
2024-04-04 12:02:57 -0300

    Add files via upload

---
+++ ajudantes.php
@@ -0,0 +1,18 @@
+<?php
+...
+function traduz_concluida($concluida)
+{
+if ($concluida == 1) {
+return 'Sim';
+}
+return 'Não';
+}
+
+
+function traduz_concluida($concluida)
+{
+if ($concluida == 1) {
+return 'Sim';
+}
+return 'Não';
+}

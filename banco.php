d5b794bfe35c39d0999610bdec7de31f8f18f722
Kauan Holtz <129107521+KauanHoltz@users.noreply.github.com>
2024-04-11 11:40:08 -0300

    Update banco.php

--- banco.php
+++ banco.php
@@ -1,41 +1,88 @@
 <?php
-$bdServidor = '127.0.0.1';
-$bdUsuario = 'root';
-$bdSenha = 'root';
+
+
+$bdServidor = 'localhost';
+$bdUsuario = 'sistematarefa';
+$bdSenha = 'sistema';
 $bdBanco = 'tarefas';
 
+
 $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+
 
 if (mysqli_connect_errno($conexao)) {
     echo "Problemas para conectar no banco. Verifique os dados!";
     die();
 }
 
+
 function buscar_tarefas($conexao)
 {
-$sqlBusca = 'SELECT * FROM tarefas';
-$resultado = mysqli_query($conexao, $sqlBusca);
+    $sqlBusca = 'SELECT * FROM tarefas';
+    $resultado = mysqli_query($conexao, $sqlBusca);
 
-$tarefas = array();
 
-while ($tarefa = mysqli_fetch_assoc($resultado)) {
-$tarefas[] = $tarefa;
+    $tarefas = array();
+
+
+    while ($tarefa = mysqli_fetch_assoc($resultado)) {
+        $tarefas[] = $tarefa;
+    }
+
+
+    return $tarefas;
 }
 
-return $tarefas;
+
+function buscar_tarefa($conexao, $id)
+{
+    $sqlBusca = 'SELECT * FROM tarefas WHERE id = ' . $id;
+    $resultado = mysqli_query($conexao, $sqlBusca);
+    return mysqli_fetch_assoc($resultado);
 }
+
 
 function gravar_tarefa($conexao, $tarefa)
 {
-$sqlGravar = "
-INSERT INTO tarefas
-(nome, descricao, prioridade)
-VALUES
-(
-'{$tarefa['nome']}',
-'{$tarefa['descricao']}',
-{$tarefa['prioridade']}
-)
-";
-mysqli_query($conexao, $sqlGravar);
+    $sqlGravar = "
+        INSERT INTO tarefas
+        (nome, descricao, prioridade, prazo, concluida)
+        VALUES
+        (
+            '{$tarefa['nome']}',
+            '{$tarefa['descricao']}',
+            {$tarefa['prioridade']},
+            '{$tarefa['prazo']}',
+            {$tarefa['concluida']}
+        )
+    ";
+
+
+    mysqli_query($conexao, $sqlGravar);
+}
+
+
+function editar_tarefa($conexao, $tarefa)
+{
+    $sqlEditar = "
+        UPDATE tarefas SET
+            nome = '{$tarefa['nome']}',
+            descricao = '{$tarefa['descricao']}',
+            prioridade = {$tarefa['prioridade']},
+            prazo = '{$tarefa['prazo']}',
+            concluida = {$tarefa['concluida']}
+        WHERE id = {$tarefa['id']}
+    ";
+
+
+    mysqli_query($conexao, $sqlEditar);
+}
+
+
+function remover_tarefa($conexao, $id)
+{
+    $sqlRemover = "DELETE FROM tarefas WHERE id = {$id}";
+
+
+    mysqli_query($conexao, $sqlRemover);
 }
